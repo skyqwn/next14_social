01728557abdc20e2e364f451593af7364ca6e32c
kimba <skyqwn@gmail.com>
2024-07-22 15:22:29 +0900

    prisma

--- lib/client.ts
+++ lib/client.ts
@@ -1,9 +1,15 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = new PrismaClient();
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
